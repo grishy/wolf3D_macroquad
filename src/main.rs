da49bb00fad09f42b5cab51e5cb4e9fc30967577
Sergey G <mailgrishy@gmail.com>
2023-02-28 00:40:31 +0400

    map 16x16

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 use macroquad::prelude::*;
 
-const MAP: [[usize; 8]; 8] = [
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 0, 1, 1, 1],
-    [1, 0, 1, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
+const MAP: [[usize; 16]; 16] = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
-const SIZE_BOCK: f32 = 64.0;
-
-static mut STATE: State = State {
-    player_x: 1.5,
-    player_y: 1.5,
-    player_angle: 0.0,
-};
+const SIZE_BOCK: f32 = 32.0;
 
 struct State {
     player_x: f32,
@@ -27,6 +29,12 @@ struct State {
 
 #[macroquad::main("Wolf3D Macroquad")]
 async fn main() {
+    let mut STATE: State = State {
+        player_x: 200.0,
+        player_y: 200.0,
+        player_angle: 0.0,
+    };
+
     loop {
         clear_background(macroquad::color::Color::from_rgba(224, 248, 207, 255));
 
@@ -37,19 +45,22 @@ async fn main() {
                         let start_x = x as f32 * SIZE_BOCK;
                         let start_y = y as f32 * SIZE_BOCK;
 
-                        draw_rectangle(start_x, start_y, SIZE_BOCK, SIZE_BOCK, GREEN);
+                        draw_rectangle(
+                            start_x,
+                            start_y,
+                            SIZE_BOCK,
+                            SIZE_BOCK,
+                            macroquad::color::Color::from_rgba(48, 104, 80, 255),
+                        );
                     }
                     0 => {}
                     _ => panic!("wrong number in map"),
                 }
-
-                // let v = point_in_wall(x, y);
-                // println!("{x},{y}= {v}");
             }
         }
+
         // draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        // draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        // draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        draw_circle(STATE.player_x, STATE.player_y, 15.0, YELLOW);
 
         next_frame().await
     }
